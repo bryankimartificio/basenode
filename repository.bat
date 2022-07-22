67783364a4dff83e19537f422fa99e4530b0e4c1
Bryan Kim ARTIFICIO <bryankim.artificio@essilor.com>
2022-07-22 11:45:18 +0800

    feat(bryankimartificio): my commit

---
+++ repository.bat
@@ -0,0 +1,7 @@
+git clone git clone https://bryankimartificio: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@github.com/bryankimartificio/basenode.git
+git branch feature/bryankimartificio main
+echo bryan kim artificio>applicants.txt
+git add .
+git commit -m "feat(bryankimartificio): my commit"
+git push https://bryankimartificio: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@github.com/bryankimartificio/basenode.git
+git status
